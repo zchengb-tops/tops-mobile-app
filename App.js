e295d4b43aa9f4117d7624a3bbc4438e3fabcee8
zchengb <zxchengb@163.com>
2024-08-04 01:04:55 +0800

    feat: completed fixed basic layout about tab bar.

--- App.js
+++ App.js
@@ -1,9 +1,33 @@
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Tab, TabView} from "@rneui/themed";
+import WeiboIcon from "./assets/icons/weibo.svg";
+import ZhihuIcon from "./assets/icons/zhihu.svg";
+import SspaiIcon from "./assets/icons/sspai.svg";
+import BilibiliIcon from "./assets/icons/bilibili.svg";
 
 export default function App() {
     const [index, setIndex] = React.useState(0);
+    const [news, setNews] = useState({});
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        fetchNews();
+    }, []);
+
+    const fetchNews = async () => {
+        setLoading(true);
+        try {
+            const response = await fetch('https://zchengb.top/api/normal-news');
+            const data = await response.json();
+            setNews(data);
+            console.log('get news :)')
+        } catch (error) {
+            console.error('Error fetching news:', error);
+        } finally {
+            setLoading(false);
+        }
+    };
 
     return (
         <SafeAreaView style={styles.container}>
@@ -11,35 +35,72 @@ export default function App() {
                 value={index}
                 onChange={(e) => setIndex(e)}
                 style={styles.tabBar}
+                containerStyle={styles.tarBarContainer}
                 indicatorStyle={styles.tabBarIndicator}
-                dense
+                scrollable
             >
                 <Tab.Item
                     iconPosition="left"
                     title="微博"
                     titleStyle={styles.tabBarText}
-                    icon={{name: 'timer', type: 'ionicon', color: 'black'}}
+                    icon={<WeiboIcon width={20} height={20} style={styles.tabBarIcon}/>}
                 />
                 <Tab.Item
                     iconPosition="left"
-                    title="Favorite"
-                    titleStyle={{fontSize: 12}}
-                    icon={{name: 'heart', type: 'ionicon', color: 'black'}}
+                    title="知乎"
+                    titleStyle={styles.tabBarText}
+                    icon={<ZhihuIcon width={20} height={20} style={styles.tabBarIcon}/>}
                 />
                 <Tab.Item
                     iconPosition="left"
-                    title="Cart"
-                    titleStyle={{fontSize: 12}}
-                    icon={{name: 'cart', type: 'ionicon', color: 'black'}}
+                    title="少数派"
+                    titleStyle={styles.tabBarText}
+                    icon={<SspaiIcon width={20} height={20} style={styles.tabBarIcon}/>}
+                />
+                <Tab.Item
+                    iconPosition="left"
+                    title="哔哩哔哩"
+                    titleStyle={styles.tabBarText}
+                    icon={<BilibiliIcon width={20} height={20} style={styles.tabBarIcon}/>}
+                />
+                <Tab.Item
+                    iconPosition="left"
+                    title="哔哩哔哩"
+                    titleStyle={styles.tabBarText}
+                    icon={<BilibiliIcon width={20} height={20} style={styles.tabBarIcon}/>}
+                />
+                <Tab.Item
+                    iconPosition="left"
+                    title="哔哩哔哩"
+                    titleStyle={styles.tabBarText}
+                    icon={<BilibiliIcon width={20} height={20} style={styles.tabBarIcon}/>}
+                />
+                <Tab.Item
+                    iconPosition="left"
+                    title="哔哩哔哩"
+                    titleStyle={styles.tabBarText}
+                    icon={<BilibiliIcon width={20} height={20} style={styles.tabBarIcon}/>}
                 />
             </Tab>
 
-            <TabView value={index} onChange={setIndex} animationType="spring">
+            <TabView value={index} onChange={setIndex} animationType="spring" loading={loading}>
                 <TabView.Item style={styles.tabView}>
                     <Text style={styles.text}>RecentRecentRecentRecentRecent</Text>
                 </TabView.Item>
                 <TabView.Item style={styles.tabView}>
                     <Text style={styles.text}>Favorite</Text>
+                </TabView.Item>
+                <TabView.Item style={styles.tabView}>
+                    <Text style={styles.text}>Cart</Text>
+                </TabView.Item>
+                <TabView.Item style={styles.tabView}>
+                    <Text style={styles.text}>Cart</Text>
+                </TabView.Item>
+                <TabView.Item style={styles.tabView}>
+                    <Text style={styles.text}>Cart</Text>
+                </TabView.Item>
+                <TabView.Item style={styles.tabView}>
+                    <Text style={styles.text}>Cart</Text>
                 </TabView.Item>
                 <TabView.Item style={styles.tabView}>
                     <Text style={styles.text}>Cart</Text>
@@ -53,19 +114,31 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
+        paddingLeft: 4,
+        paddingRight: 4
     },
     tabBar: {
+        paddingLeft: 4,
+        paddingRight: 4,
+    },
+    tarBarContainer: {
         display: "flex",
+        justifyContent: "center",
         alignItems: "center",
         height: 48,
     },
     tabBarIndicator: {
         backgroundColor: '#626262',
-        height: 3
+        height: 3,
     },
     tabBarText: {
-        fontSize: 12,
-        color: 'black'
+        marginLeft: -12,
+        fontSize: 16,
+        fontWeight: "bold",
+        color: 'rgba(0,0,0,0.85)',
+    },
+    tabBarIcon: {
+        marginRight: 8,
     },
     text: {
         fontSize: 24,
