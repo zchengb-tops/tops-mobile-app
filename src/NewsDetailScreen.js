6afe9688dda17451a37a14c9b2e2aba497f8b4cd
xiaobin <xiaobin.zheng@thoughtworks.com>
2024-08-29 20:29:28 +0800

    fix: fix news detail screen blank issue.

--- src/NewsDetailScreen.js
+++ src/NewsDetailScreen.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import {WebView} from "react-native-webview";
+import {ActivityIndicator, StyleSheet, View} from "react-native";
 
 export const NewsDetailScreen = ({route}) => {
     const {url} = route.params;
-    return <WebView style={{flex: 1}} source={{uri: url}}
-                    allowsFullscreenVideo={false}
-                    javaScriptEnabled={true}
-                    allowsInlineMediaPlayback={true}
-    />;
+    const [loading, setLoading] = useState(true);
+
+    return <View style={styles.container}>
+        {
+            loading && <View style={styles.loadingContainer}><ActivityIndicator size={"large"}/></View>
+        }
+
+        <WebView source={{uri: url}}
+                 originWhitelist={['*']}
+                 allowsFullscreenVideo={false}
+                 javaScriptEnabled={true}
+                 allowsInlineMediaPlayback={true}
+                 onLoadEnd={(e) => {
+                     setLoading(false);
+                 }}
+        />
+    </View>;
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    loadingContainer: {
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});
