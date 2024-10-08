f0187b4bff3d6025871a4f36788fc1b7134fda69
zchengb <zxchengb@163.com>
2024-10-08 22:57:14 +0800

    refactor: optimize stock data refresh method.

--- src/tabs/Stock.js
+++ src/tabs/Stock.js
@@ -1,15 +1,62 @@
-import React, {useEffect, useRef, useState} from 'react';
-import {Dimensions, ScrollView, StyleSheet, Text, View} from 'react-native';
+import React, {useContext, useEffect, useRef, useState} from 'react';
+import {Dimensions, RefreshControl, ScrollView, StyleSheet, Text, View} from 'react-native';
 import * as echarts from 'echarts/core';
 import {GridComponent, TooltipComponent, VisualMapComponent} from 'echarts/components';
 import {SvgChart, SVGRenderer} from '@wuba/react-native-echarts';
 import {BarChart, TreemapChart} from "echarts/charts";
 import {useSafeAreaInsets} from "react-native-safe-area-context";
+import {NewsContext} from "../../utils/NewsProvider";
+import {globalStyles} from "../globalStyle";
 
 echarts.use([SVGRenderer, GridComponent, BarChart, TreemapChart, VisualMapComponent, TooltipComponent]);
 
 export const Stock = () => {
+    const {allNews, refreshing, refreshNews} = useContext(NewsContext);
     const [acquisitionTime, setAcquisitionTime] = useState(null);
+    const [chartOption, setChartOption] = useState({
+        label: {
+            formatter: (value) => {
+                return `${value.name}\n${value.value[2]}%`;
+            },
+            fontSize: 12
+        },
+        series: {
+            itemStyle: {
+                borderColor: "white",
+                gapWidth: 0.5
+            },
+            levels: [
+                {
+                    itemStyle: {}
+                }
+            ],
+            type: "treemap",
+            top: 8,
+            left: 8,
+            right: 8,
+            bottom: 8,
+            roam: false,
+            nodeClick: false,
+            visualDimension: 2,
+            data: [],
+            breadcrumb: {
+                show: false
+            },
+            label: {
+                show: true,
+            }
+        },
+        visualMap: {
+            show: false,
+            precision: 2,
+            type: "continuous",
+            min: -4.0,
+            max: 4.0,
+            inRange: {
+                color: ["#62cda4", "#4d9e81", "#417d6b", "#334e49", "#543b3f", "#7a494b", "#c76663", "#ed7470"]
+            }
+        }
+    });
     const skiaRef = useRef(null);
     const insets = useSafeAreaInsets();
     const screenWidth = Dimensions.get('window').width;
@@ -37,73 +84,34 @@ export const Stock = () => {
             .padStart(2, '0')}`;
     }
 
+    const renderChart = () => {
+        let chart;
+        if (skiaRef.current) {
+            chart = echarts.init(skiaRef.current, 'light', {
+                renderer: 'svg',
+                width: screenWidth,
+                height: screenHeight - insets.top - 18 - 100 - insets.bottom
+            });
+            chart.setOption(chartOption);
+        }
+    };
+
     useEffect(() => {
-        const fetchDataAndRenderChart = async () => {
-            const option = {
-                label: {
-                    formatter: (value) => {
-                        return `${value.name}\n${value.value[2]}%`;
-                    },
-                    fontSize: 12
-                },
-                series: {
-                    itemStyle: {
-                        borderColor: "white",
-                        gapWidth: 0.75
-                    },
-                    levels: [
-                        {
-                            itemStyle: {}
-                        }
-                    ],
-                    type: "treemap",
-                    top: 8,
-                    left: 8,
-                    right: 8,
-                    bottom: 8,
-                    roam: false,
-                    nodeClick: false,
-                    visualDimension: 2,
-                    data: [],
-                    breadcrumb: {
-                        show: false
-                    },
-                    label: {
-                        show: true,
-                    }
-                },
-                visualMap: {
-                    show: false,
-                    precision: 2,
-                    type: "continuous",
-                    min: -4.0,
-                    max: 4.0,
-                    inRange: {
-                        color: ["#62cda4", "#4d9e81", "#417d6b", "#334e49", "#543b3f", "#7a494b", "#c76663", "#ed7470"]
-                    }
-                }
-            };
+        const newChartOption = {...chartOption};
+        const stocks = allNews['stock'];
+        newChartOption.series.data = stocks;
+        setChartOption(newChartOption);
+        setAcquisitionTime(stocks[0]?.acquisitionTime);
 
-            const response = await fetch('https://zchengb.top/api/news/stock');
-            const data = await response.json();
-            setAcquisitionTime(data.acquisitionTime);
-            option.series.data = data.stocks;
+        renderChart();
+    }, [allNews]);
 
-            let chart;
-            if (skiaRef.current) {
-                chart = echarts.init(skiaRef.current, 'light', {
-                    renderer: 'svg',
-                    width: screenWidth,
-                    height: screenHeight - insets.top - 18 - 100 - insets.bottom
-                });
-                chart.setOption(option);
-            }
-        };
-
-        fetchDataAndRenderChart();
-    }, []);
-
-    return <ScrollView style={{flex: 1}} contentContainerStyle={{alignItems: 'center', justifyContent: 'center'}}>
+    return <ScrollView
+        contentContainerStyle={styles.stockContentWrapper}
+        refreshControl={
+            <RefreshControl style={globalStyles.refreshControl} refreshing={refreshing} onRefresh={refreshNews}/>
+        }
+    >
         <View style={styles.timeTipsWrapper}>
             <Text style={styles.timeTipsText}>*更新于 {formatDate(acquisitionTime)} 中国</Text>
         </View>
@@ -112,6 +120,16 @@ export const Stock = () => {
 };
 
 const styles = StyleSheet.create({
+    stockContentWrapper: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    loadingView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
     timeTipsWrapper: {
         marginTop: 8,
         marginRight: 12,
