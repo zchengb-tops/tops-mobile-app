84e0b9efa422b296d25f810c169776524955abee
zchengb <zxchengb@163.com>
2024-12-26 00:05:57 +0800

    feat: add News API module with functions to fetch RSS and normal news

---
+++ src/apis/News.js
@@ -0,0 +1,46 @@
+import { request } from "../request";
+
+export const sendVerificationCode = (email) => {
+    return request('/sign-in-verification-code/email', {
+        method: 'POST',
+        body: { email }
+    });
+};
+
+export const signIn = (email, verificationCode) => {
+    return request('/user/sign-in', {
+        method: 'POST',
+        body: {
+            email,
+            verificationCode
+        }
+    });
+};
+
+export const getUserInfo = () => {
+    return request('/user/me', {
+        method: 'GET'
+    });
+};
+
+
+export const getUserNewsChannelConfig = () => {
+    return request('/user/news-channel-config', {
+        method: 'GET'
+    });
+};
+
+export const getUserNewsChannelConfigCurrentVersion = () => {
+    return request('/user/news-channel-config/version', {
+        method: 'GET'
+    });
+};
+
+export const updateUserNewsChannelConfig = (channelSettings) => {
+    return request('/user/news-channel-config', {
+        method: 'POST',
+        body: {
+            "content": JSON.stringify(channelSettings)
+        }
+    });
+};
